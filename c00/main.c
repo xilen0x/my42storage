78281716b20f8a66ea79b6e1e3c4cf19c5122696
Carlos Astorga <castorga@cbr9s3.42barcelona.com>
2023-04-05 09:11:33 +0200

    updated

--- c00/main.c
+++ c00/main.c
@@ -1,6 +1,6 @@
 //#include "test6b.c"
 //#include "ex06/ft_print_comb2.c"
-#include "ex05/ft_print_comb.c"
+//#include "ex05/ft_print_comb.c"
 //#include "ex04/ft_is_negative.c"
 //#include "ex03/ft_print_numbers.c"
 //#include "ex02/ft_print_reverse_alphabet.c"
@@ -9,9 +9,9 @@
 
 int	main(void)
 {
-    //ft_print_comb2();
-    ft_print_comb();
-    //ft_is_negative(-5);
+	//ft_print_comb2();
+	//ft_print_comb();
+	//ft_is_negative(-5);
 	//ft_print_numbers();
 	//ft_print_reverse_alphabet();
 	//ft_print_alphabet();
