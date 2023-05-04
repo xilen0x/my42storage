2f5113a129352d25ce72d8c2beb2559c710fdc0e
Carlos Astorga <castorga@car10s6.42barcelona.com>
2023-05-04 10:51:08 +0200

    update Readme

---
+++ cursus/project01/ft_isdigit.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: castorga <castorga@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 17:04:19 by castorga          #+#    #+#             */
+/*   Updated: 2023/05/03 17:04:22 by castorga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isdigit(int num)
+{
+	if (num >= '0' && num <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+#include <stdio.h>
+#include <ctype.h>
+
+int	main(void)
+{
+	char	c, d;
+
+	c = '2';
+	d = 'a';
+	printf("\nWhen digit is passed: %d", isdigit(c));
+	printf("\nWhen non-digit is passed: %d", isdigit(d));
+	printf("\n-------\n");
+	printf("\nWhen digit is passed: %d", ft_isdigit(c));
+	printf("\nWhen non-digit is passed: %d", ft_isdigit(d));
+	return (0);
+}
