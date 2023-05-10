54c014e3c5c04664becf1533681c1da08a634007
Carlos Astorga <castorga@cbr10s1.42barcelona.com>
2023-05-10 19:53:15 +0200

    update miercoles

--- cursus/project01/ft_memmove.c
+++ cursus/project01/ft_memmove.c
@@ -10,4 +10,53 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned int	i;
+	unsigned char	*psrc;
+	unsigned char	*pdst;
+
+	if (dst == NULL && src == NULL)
+		return (NULL);
+	psrc = (unsigned char *)src;
+	pdst = (unsigned char *)dst;
+	if (pdst > psrc)
+	{
+		i = len;
+		while (i > 0)
+		{
+			pdst[i - 1] = psrc[i - 1];
+			i--;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < len)
+		{
+			pdst[i] = psrc[i];
+			i++;
+		}
+	}
+	return (dst);
+}
+
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+	// printf("\n---------memmove-------------*\n");
+	char src[] = "almost every programmer should know memmove!";
+	//char dst[] = "  ";
+	//printf("%s\n", (char *)memmove(dst, src, 3));
+
+	char dst2[] = "  ";
+	printf("\n---------ft_memmove----------*\n");
+	printf("%s", (char *)ft_memmove(dst2, src, 3));
+
+	return 0;
+}*/
