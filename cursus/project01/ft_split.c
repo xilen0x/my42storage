f2deb38341703f7f7b8d06895663753c6187fa2f
Carlos Astorga <carlos.astorgax@gmail.com>
2023-05-30 15:25:48 +0200

    split ended

--- cursus/project01/ft_split.c
+++ cursus/project01/ft_split.c
@@ -44,7 +44,7 @@ El array debe terminar con un puntero NULL.
 // 	return (temp);
 // }
 
-
+/*
 //----------------funcion que reserva memoria----------------
 static char	*ft_my_malloc(const char *src, size_t len)
 {
@@ -53,14 +53,14 @@ static char	*ft_my_malloc(const char *src, size_t len)
 	dst = malloc(len + 1);
 	if (dst == NULL)
 		return (NULL);
-	ft_memcpy(dst, src, len);
+	memcpy(dst, src, len);
 	dst[len] = '\0';
 	return (dst);
 }
 
 //---------funcion que encuentra todos los 'c' y retorna sus indices---------
 
-static int	*ft_find_index_of_c(char *s, int c, size_t n, size_t *count)
+static int	*ft_find_index_of_c(char *s, int c, size_t n)
 {
 	unsigned char	c_to_find;
 	size_t			i;
@@ -70,17 +70,16 @@ static int	*ft_find_index_of_c(char *s, int c, size_t n, size_t *count)
 	c_to_find = (unsigned char)c;
 	i = 0;
 	j = 0;
-	indices = NULL;
-	*count = 0;
+	indices = 0;
 	while (i < n)
 	{
 		if (s[i] == c_to_find)
-			(*count)++;
+			(*indices)++;
 		i++;
 	}
-	if (*count == 0)
+	if (*indices == 0)
 		return (NULL);
-	indices = (int *)malloc((*count) * sizeof(int));
+	indices = (int *)malloc((*indices) * sizeof(int));
 	i = 0;
 	while (i < n)
 	{
@@ -97,46 +96,96 @@ static int	*ft_find_index_of_c(char *s, int c, size_t n, size_t *count)
 //----------------funcion principal split----------------
 char	**ft_split(char const *s, char c)
 {
-	size_t	count;
 	size_t	i;
 	int		*indices;
 	char	**result;
 	size_t	start;
 	size_t	len;
 
-	indices = ft_find_index_of_c((char *)s, c, ft_strlen(s), &count);
-	result = (char **)malloc((count + 2) * sizeof(char *));
+	indices = ft_find_index_of_c((char *)s, c, strlen(s));
+    result = (char **)malloc((2) * sizeof(char *));
 	if (!result)
 		return (NULL);
 	i = 0;
 	start = 0;
-	while (i < count)
+	while (s)
 	{
 		len = indices[i] - start;
 		result[i] = ft_my_malloc(s + start, len);
 		start = indices[i] + 1;
 		i++;
 	}
-	result[i] = ft_my_malloc(s + start, ft_strlen(s) - start);
-	result[i + 1] = 0; // final de array de punteros
+	//result[i] = ft_my_malloc(s + start, strlen(s) - start);
+	result[i] = ft_my_malloc(s + start, indices[i-1] - start + 1); // Actualizar la longitud de la última subcadena
+	result[i + 1] = NULL; // final de array de punteros
 	free(indices);
 	return (result);
+return (NULL);
+}
+*/
+
+static size_t	ft_toklen(const char *s, char c)
+{
+	size_t	ret;
+
+	ret = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			++ret;
+			while (*s && *s != c)
+				++s;
+		}
+		else
+			++s;
+	}
+	return (ret);
 }
 
+char	**ft_split(const char *s, char c)
+{
+	char	**ret;
+	size_t	i;
+	size_t	len;
+
+	if (!s)
+		return (0);
+	i = 0;
+	ret = malloc(sizeof(char *) * (ft_toklen(s, c) + 1));
+	if (!ret)
+		return (0);
+	while (*s)
+	{
+		if (*s != c)
+		{
+			len = 0;
+			while (*s && *s != c && ++len)
+				++s;
+			ret[i++] = ft_substr(s - len, 0, len);
+		}
+		else
+			++s;
+	}
+	ret[i] = 0;
+	return (ret);
+}
+
+/*
 int	main(void)
 {
 	// char *str0 = "hola mundo 42 Chile";
 	// printf("Se espera: , 'hola', 'mundo', '42', 'Chile'\n");
 	int character = ' ';
-	//char **result = ft_split(str0, character);
+	// char **result = ft_split(str0, character);
 
-	char *str1 = "      split       this for   me  !       ";
-	printf("Se espera: , 'split', 'this', 'for', 'me', '!'\n");
-	char **result1 = ft_split(str1, character);
+	//char *str1 = "      split       this for   me  !       ";
+	//printf("Se espera: , 'split', 'this', 'for', 'me', '!'\n");
+	//char **result1 = ft_split(str1, character);
 
-	// char *str2 = "                  olol";
-	// printf("Se espera: , 'olol'\n");
-	// char **result2 = ft_split(str2, character);
+	char *str2 = "                  olol";
+	printf("Se espera: , 'olol'\n");
+	char **result2 = ft_split(str2, character);
 
 	// char *str3 = "\0aa\0bbb";
 	// int character3 = '\0';
@@ -157,16 +206,15 @@ int	main(void)
 	// 		i++;
 	// 	}
 	// }
-	if (result1)
+	if (result2)
 	{
 		size_t i = 0;
-		while (result1[i])
+		while (result2[i])
 		{
-			printf("%s\n", result1[i]);
+			printf("%s\n", result2[i]);
 			i++;
 		}
 	}
-
         // Liberar la memoria asignada a las subcadenas y al arreglo de punteros
         // i = 0;
         // while (result[i])
@@ -177,3 +225,4 @@ int	main(void)
         // free(result);
     return (0);
 }
+*/
