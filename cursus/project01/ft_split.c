999ab3dfd20bd058888d9dba3a1849cf4a3e5da6
Carlos Astorga <carlos.astorgax@gmail.com>
2023-05-30 11:31:10 +0200

    split updated

--- cursus/project01/ft_split.c
+++ cursus/project01/ft_split.c
@@ -34,77 +34,102 @@ El array debe terminar con un puntero NULL.
 #include "libft.h"
 
 //----------------funcion que reserva memoria----------------
-static int	*ft_my_malloc(size_t *count)
-{
-	int	*temp;
+// static int	*ft_my_malloc(size_t *count)
+// {
+// 	int	*temp;
 
-	temp = (int *)malloc((*count) * sizeof(int));
-	if (temp == NULL)
-		return (NULL);
-	return (temp);
+// 	temp = (int *)malloc((*count) * sizeof(int));
+// 	if (temp == NULL)
+// 		return (NULL);
+// 	return (temp);
+// }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//----------------funcion que reserva memoria----------------
+static char *ft_my_strdup(const char *src, size_t len)
+{
+    char *dst = malloc(len + 1);
+    if (dst == NULL)
+        return NULL;
+    memcpy(dst, src, len);
+    dst[len] = '\0';
+    return dst;
 }
 
 //---------funcion que encuentra todos los 'c' y retorna sus indices---------
 
-static int	*ft_find_index_of_c(char *s, int c, size_t n, size_t *count)
+static int *ft_find_index_of_c(char *s, int c, size_t n, size_t *count)
 {
-	unsigned char	c_to_find;
-	size_t			i;
-	size_t			j;
-	int				*indices;
-	int				*temp;
+    unsigned char c_to_find = (unsigned char)c;
+    size_t i = 0;
+    size_t j = 0;
+    int *indices = NULL;
 
-	c_to_find = (unsigned char)c;
-	i = 0;
-	j = 0;
-	indices = NULL;
-	*count = 0;
-	while (i < n)
-	{
-		if (s[i] == c_to_find)
-		{
-			(*count)++;
-			temp = ft_my_malloc(count);
-			ft_memcpy(temp, indices, j * sizeof(int));
-			indices = temp;
-			indices[j] = i;
-			j++;
-		}
-		i++;
-	}
-	return (indices);
+    *count = 0;
+    while (i < n)
+    {
+        if (s[i] == c_to_find)
+            (*count)++;
+        i++;
+    }
+
+    if (*count == 0)
+        return NULL;
+
+    indices = (int *)malloc((*count) * sizeof(int));
+    if (indices == NULL)
+        return NULL;
+
+    i = 0;
+    while (i < n)
+    {
+        if (s[i] == c_to_find)
+        {
+            indices[j] = i;
+            j++;
+        }
+        i++;
+    }
+
+    return indices;
 }
 
 //----------------funcion principal split----------------
-char	**ft_split(char const *s, char c)
+char **ft_split(char const *s, char c)
 {
-	size_t			count;
-	size_t			i;
-	int				*indices;
-	char			**result;
+    size_t count;
+    size_t i;
+    int *indices;
+    char **result;
 
-	indices = ft_find_index_of_c((char *)s, c, ft_strlen(s), &count);
-	result = (char **)malloc((count + 2) * sizeof(char *));
-	if (!result)
-		return (NULL);
-	i = 0;
-	while (indices && i <= count)
-	{
-		//result[i] = ft_substr(s, indices[i], (indices[i + 1]) - indices[1]);
-		//result[i] = ft_strdup((char *)&s[indices[i]]);
-		result[i] = strndup(s + indices[i], indices[i + 1] - indices[i] - 1);
-		i++;
-	}
-	result[i] = NULL;//final de array de punteros
-	free(indices);
-	return (result);
+    indices = ft_find_index_of_c((char *)s, c, strlen(s), &count);
+    result = (char **)malloc((count + 2) * sizeof(char *));
+    if (!result)
+        return NULL;
+
+    i = 0;
+    size_t start = 0;
+    while (i < count)
+    {
+        size_t len = indices[i] - start;
+        result[i] = ft_my_strdup(s + start, len);
+        start = indices[i] + 1;
+        i++;
+    }
+    result[i] = ft_my_strdup(s + start, strlen(s) - start);
+    result[i + 1] = NULL; // final de array de punteros
+
+    free(indices);
+    return result;
 }
-
 
 int main()
 {
-    char *str = "0123-567-9!";
-    int character = '-';
+    char *str = "hola mundo 42 Chile";
+    int character = ' ';
     char **result = ft_split(str, character);
     if (result)
     {
@@ -115,14 +140,15 @@ int main()
             printf("%s\n", result[i]);
             i++;
         }
+        
         // Liberar la memoria asignada a las subcadenas y al arreglo de punteros
-        i = 0;
-        while (result[i])
-        {
-            free(result[i]);
-            i++;
-        }
-        free(result);
+        // i = 0;
+        // while (result[i])
+        // {
+        //     free(result[i]);
+        //     i++;
+        // }
+        // free(result);
     }
 
     return 0;
