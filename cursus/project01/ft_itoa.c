8b69b3b1a3afecec13ea4e94f22b6861ff557496
Carlos Astorga <carlos.astorgax@gmail.com>
2023-06-01 11:32:26 +0200

    update itoa - numlen ended

--- cursus/project01/ft_itoa.c
+++ cursus/project01/ft_itoa.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/*Saves the given number as a string (char array)*/
+/* Saves the given number as a string (char array) */
 
 /*
 Prototipo 
@@ -33,66 +33,34 @@ Los números negativos tienen que gestionarse.
 */
 # include "libft.h"
 
-static void	calc_size(long int n, size_t *size)
+static size_t	ft_numlen(int n)
 {
-	if (n < 0)
-		n *= -1;
-	while (n >= 10)
+	size_t	count;
+
+	count = 1;
+	while (n / 10 != 0)
 	{
-		*size += 1;
 		n /= 10;
+		count++;
 	}
-	*size += 1;
+	return (count);
 }
 
-static void	create_str(char *str, long int n, size_t size)
-{
-	size_t	i;
-	size_t	j;
-	int		c;
+// char	*ft_itoa(int n)
+// {
+// 	//int str;
 
-	i = 0;
-	j = 0;
-	if (n < 0)
-	{
-		str[0] = '-';
-		n *= -1;
-		i++;
-		j++;
-	}
-	while (size > j)
-	{
-		c = (n % 10);
-		str[size - 1] = c + '0';
-		n = n / 10;
-		i++;
-		size--;
-	}
-	str[i] = '\0';
-}
-
-char	*ft_itoa(int n)
-{
-	size_t	size;
-	char	*str;
-
-	size = 0;
-	if (n < 0)
-		size = 1;
-	calc_size(n, &size);
-	str = malloc(sizeof(char) * (size + 1));
-	if (!str)
-		return (NULL);
-	create_str(str, n, size);
-	return (str);
-}
+// 	str = ft_numlen(n);
+// 	return (str);
+// }
 
 int main()
 {
     int number = -12345;
-    char *str_number = ft_itoa(number);
+    //char *str_number = ft_itoa(number);
     printf("Number: %d\n", number);
-    printf("String: %s\n", str_number);
-    free(str_number);
+    printf("%zu", ft_numlen(number));
+	//printf("String: %s\n", str_number);
+    //free(str_number);
     return 0;
-}
+}	
