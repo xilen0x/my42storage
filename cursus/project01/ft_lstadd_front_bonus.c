9e9fd629eb21cec7ef7759d614b77d0262921644
Carlos Astorga <carlos.astorgax@gmail.com>
2023-06-07 17:16:39 +0200

    update bonus

--- cursus/project01/ft_lstadd_front_bonus.c
+++ cursus/project01/ft_lstadd_front_bonus.c
@@ -15,8 +15,8 @@ Prototipo
 void ft_lstadd_front(t_list **lst, t_list *new);
 
 Parámetros 
-lst: la dirección de un puntero al primer nodo de una lista.
-new: un puntero al nodo que añadir al principio de la lista.
+lst: puntero al primer nodo de una lista.
+new: puntero al nodo que añadir al principio de la lista.
 
 Valor devuelto 
 Nada
@@ -27,3 +27,19 @@ Ninguna
 Descripción 
 Añade el nodo ’new’ al principio de la lista ’lst’.
 */
+
+#include "libft.h"
+
+/*typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+*/
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	//se asigna el puntero *lst al campo next del nuevo elemento new
+	new->next = *lst;
+	*lst = new;
+}
