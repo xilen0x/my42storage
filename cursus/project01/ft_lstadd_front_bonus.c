2ca0ff7a1859f82881505968621701f59fcdf9cc
Carlos Astorga <carlos.astorgax@gmail.com>
2023-06-08 13:48:40 +0200

    lstadd_front_bonus ended

--- cursus/project01/ft_lstadd_front_bonus.c
+++ cursus/project01/ft_lstadd_front_bonus.c
@@ -49,6 +49,7 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	}
 }
 
+/*
 //funcion que imprime la lista
 static void	print_list(t_list *lst)
 {
@@ -62,27 +63,30 @@ static void	print_list(t_list *lst)
 	}
 	printf("\n");
 }
-
 int	main(void)
 {
-	// ------------ PRIMER NODO ------------
+	// ------------------------ PRIMER NODO ------------------------
 	int *data1 = (int *)malloc(sizeof(int));  // Asigna memoria para el contenido del nodo
 	*data1 = 42;  // Asigna el valor 42 al contenido del nodo
 	t_list *node1 = (t_list *)malloc(sizeof(t_list));  // Asigna memoria para el nodo
 	node1->content = data1;  // Asigna el contenido al nodo
 	node1->next = NULL;  // Establece el siguiente puntero del nodo como nulo
 
-	// ------------ SEGUNDO NODO ------------
+	// ------------------------ SEGUNDO NODO ------------------------
 	int *data2 = (int *)malloc(sizeof(int));  // Asigna memoria para el contenido del nodo
 	*data2 = 75;  // Asigna el valor 75 al contenido del nodo
 	t_list *node2 = (t_list *)malloc(sizeof(t_list));  // Asigna memoria para el nodo
 	node2->content = data2;  // Asigna el contenido al nodo
 	node2->next = NULL;  // Establece el siguiente puntero del nodo como nulo
 
-	ft_lstadd_front(&node1, node2);  // Agrega el segundo nodo al frente de la lista
+	// ------------ Agrega el segundo nodo al frente de la lista ------------
+	ft_lstadd_front(&node1, node2);
+
+	// ------------------------ PRINTS ------------------------
 	printf("Contenido de la lista: ");
 	print_list(node1);  // Imprime el contenido de la lista
 	printf("Contenido del nodo1: %d\n", *(int *)(node1->content));  // Imprime el contenido del nodo1
 	printf("Contenido del nodo2: %d\n", *(int *)(node1->next->content));  // Imprime el contenido del nodo2
 	return (0);
 }
+*/
