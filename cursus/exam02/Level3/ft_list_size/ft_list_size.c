7320117173d08831ee0c3b381d6608af5148ca4b
Carlos Astorga <castorga@cbr2s5.42barcelona.com>
2023-09-28 13:23:05 +0200

    update

--- cursus/exam02/Level3/ft_list_size/ft_list_size.c
+++ cursus/exam02/Level3/ft_list_size/ft_list_size.c
@@ -21,11 +21,36 @@ typedef struct    s_list
 */
 
 #include "ft_list.h"
+# include <stdlib.h>
+# include <stdio.h>
+//# include <string.h>
 
 int	ft_list_size(t_list *begin_list)
 {
 	if (begin_list == 0)
 		return (0);
 	else
-		return (1 + ft_list_size(begin_list->next));
+		return (ft_list_size(begin_list->next) + 1);
+}
+
+int	main(void)
+{
+    // Crear una lista simple
+    t_list node1, node2, node3;
+
+    node1.data = "Hola";
+    node2.data = "Mundo";
+    node3.data = "!";
+
+    node1.next = &node2;
+    node2.next = &node3;
+    node3.next = NULL;
+
+    // Calcular el tamaño de la lista usando ft_list_size
+    int size = ft_list_size(&node1);
+
+    // Imprimir el resultado
+    printf("El tamaño de la lista es: %d\n", size);
+
+    return (0);
 }
